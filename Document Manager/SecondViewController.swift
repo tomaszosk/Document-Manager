5f32b47c76cfa84f98c069ef2cc0b3b857cf30bc
tomaszosk <t.oskroba@icloud.com>
2019-10-20 22:31:08 +0200

    Table View added

--- Document Manager/SecondViewController.swift
+++ Document Manager/SecondViewController.swift
@@ -8,8 +8,27 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let docList = ["Dokument 1", "Dokument 2", "Dokument 3", "Dokument 4"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return (docList.count)
+    }
 
+
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = docList[indexPath.row]
+        
+        return(cell)
+    }
+
+    @IBOutlet weak var docTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
