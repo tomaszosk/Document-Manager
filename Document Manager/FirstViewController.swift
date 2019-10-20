0c90744f5d37c01f188170628164af7bd0bcfcde
tomaszosk <t.oskroba@icloud.com>
2019-10-20 22:16:20 +0200

    Initial Commit

---
+++ Document Manager/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Document Manager
+//
+//  Created by Tomasz Oskroba on 10/20/19.
+//  Copyright © 2019 Tomasz Oskroba. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Document Manager/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Document Manager
+//
+//  Created by Tomasz Oskroba on 10/20/19.
+//  Copyright © 2019 Tomasz Oskroba. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
