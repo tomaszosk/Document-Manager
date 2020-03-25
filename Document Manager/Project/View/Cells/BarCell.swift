6c5e75874f15901631097053e1bb03ebb86e5fa8
tomaszosk <t.oskroba@icloud.com>
2020-03-25 19:17:12 +0100

    Minor clean ups

--- Document Manager/Project/View/Cells/BarCell.swift
+++ Document Manager/Project/View/Cells/BarCell.swift
@@ -32,25 +32,13 @@ class BarCell: UICollectionViewCell {
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        addSubview(barView)
+        configure()
         
 
-//        barView.topAnchor.constraint(equalTo: topAnchor).isActive = true
-//        configure()
-        
-        barView.layer.cornerRadius = 10
-        barView.layer.masksToBounds = true
-        barView.setGradientBackground(colorOne: Colors.orange, colorTwo: Colors.brightOrange)
 
-        barHeightConstraint = barView.heightAnchor.constraint(equalToConstant: 300)
+//
 
-        barHeightConstraint?.isActive = true
-        barHeightConstraint?.constant = 100
-
-        barView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
-        barView.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
-        barView.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
-        
+//
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -73,16 +61,33 @@ class BarCell: UICollectionViewCell {
         barView.layer.masksToBounds = true
         barView.setGradientBackground(colorOne: Colors.orange, colorTwo: Colors.brightOrange)
         
+                barView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+//                configure()
+//
+//                barView.layer.cornerRadius = 10
+//                barView.layer.masksToBounds = true
+//                barView.setGradientBackground(colorOne: Colors.orange, colorTwo: Colors.brightOrange)
+        
+                barHeightConstraint = barView.heightAnchor.constraint(equalToConstant: 300)
+        
+        
+                barHeightConstraint?.isActive = true
+                barHeightConstraint?.constant = 100
+        
+                barView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+                barView.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+                barView.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
+        
         NSLayoutConstraint.activate([
             barCountLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: padding),
             barCountLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
             barCountLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
             barCountLabel.heightAnchor.constraint(equalToConstant: 20),
             
-            barView.topAnchor.constraint(equalTo: barCountLabel.bottomAnchor, constant: 12),
-            barView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
-            barView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
-            barView.heightAnchor.constraint(equalToConstant: 300),
+//            barView.topAnchor.constraint(equalTo: barCountLabel.bottomAnchor, constant: 12),
+//            barView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+//            barView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+//            barView.heightAnchor.constraint(equalToConstant: 300),
             
             barNameLabel.topAnchor.constraint(equalTo: barView.bottomAnchor, constant: 12),
             barNameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
